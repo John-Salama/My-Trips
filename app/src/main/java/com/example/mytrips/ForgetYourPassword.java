ed556952cc162dcab0fa2bfab48254297808ed8b
Youssef Ahmed Hosny <68037436+Usf-Darwish50@users.noreply.github.com>
2022-10-04 02:26:20 +0200

    test22

--- app/src/main/java/com/example/mytrips/ForgetYourPassword.java
+++ app/src/main/java/com/example/mytrips/ForgetYourPassword.java
@@ -10,5 +10,6 @@ public class ForgetYourPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_your_password);
+        ///aaa
     }
 }
