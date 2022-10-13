1285bd99b08de3a561f1662e7b84bff477fb2b89
Youssef Ahmed Hosny <68037436+Usf-Darwish50@users.noreply.github.com>
2022-10-13 19:17:06 +0200

    1st try not working

---
+++ app/src/main/java/com/example/mytrips/LoginPage.java
@@ -0,0 +1,102 @@
+package com.example.mytrips;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginPage extends AppCompatActivity {
+    DatabaseReference db = FirebaseDatabase.getInstance().getReferenceFromUrl("https://my-trips-66039-default-rtdb.firebaseio.com/");
+    private TextView mRegistrationView;
+    private TextView mForgotPasswordView;
+    private TextView mEmailTextView;
+    private TextView mPasswordTextView;
+    private Button mLoginBtn;
+    private String mEmail;
+    private String mPassword;
+    Intent addTrip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //initializeViewComponent();
+        //goToRegistration();
+        //goToForgot();
+       mLoginBtn.setOnClickListener(view -> {
+            addTrip = new Intent(LoginPage.this, MainActivity.class);
+            startActivity(addTrip);
+        });
+         //login();
+    }
+
+    private void initializeViewComponent() {
+        mRegistrationView = findViewById(R.id.register_text);
+        mForgotPasswordView = findViewById(R.id.forgot_password_text);
+        mEmailTextView = findViewById(R.id.login_email_text);
+        mPasswordTextView = findViewById(R.id.login_password_text);
+        mLoginBtn = findViewById(R.id.login_btn);
+    }
+
+    private void login() {
+        mLoginBtn.setOnClickListener(v -> {
+            getLoginData();
+            if(mEmail.equals("")|| mPassword.equals("")){
+                Toast.makeText(LoginPage.this,"please fill all the fields",Toast.LENGTH_LONG).show();
+            }else{
+                db.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.hasChild(mEmail)){
+                            String getPass = snapshot.child(mEmail).child("password").getValue(String.class);
+                            assert getPass != null;
+                            if(getPass.equals(mPassword)){
+                                addTrip = new Intent(LoginPage.this,UpcomingPage.class);
+                                startActivity(addTrip);
+                            }else{
+                                mPasswordTextView.setError("wrong password");
+                                mPasswordTextView.requestFocus();
+                            }
+                        }
+                        else{
+                                mEmailTextView.setError("wrong email");
+                                mEmailTextView.requestFocus();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {}
+                });
+            }
+        });
+    }
+
+    private void getLoginData() {
+        mEmail = mEmailTextView.getText().toString().replace(".",",");
+        mPassword = mPasswordTextView.getText().toString();
+    }
+
+    private void goToForgot() {
+        mForgotPasswordView.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginPage.this,ForgetYourPassword.class);
+            startActivity(intent);
+        });
+    }
+
+    private void goToRegistration() {
+        mRegistrationView.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginPage.this,RegisterPage.class);
+            startActivity(intent);
+        });
+    }
+}
--- app/src/main/java/com/example/mytrips/MainActivity.java
+++ app/src/main/java/com/example/mytrips/MainActivity.java
@@ -1,102 +1,89 @@
 package com.example.mytrips;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.example.mytrips.databinding.ActivityNavigationBinding;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
-    DatabaseReference db = FirebaseDatabase.getInstance().getReferenceFromUrl("https://my-trips-66039-default-rtdb.firebaseio.com/");
-    private TextView mRegistrationView;
-    private TextView mForgotPasswordView;
-    private TextView mEmailTextView;
-    private TextView mPasswordTextView;
-    private Button mLoginBtn;
-    private String mEmail;
-    private String mPassword;
-    Intent addTrip;
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityNavigationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initializeViewComponent();
-        goToRegistration();
-        goToForgot();
-       /* mLoginBtn.setOnClickListener(view -> {
-            addTrip = new Intent(MainActivity.this,AddNotePage.class);
-            startActivity(addTrip);
-        });*/
-         login();
-    }
 
-    private void initializeViewComponent() {
-        mRegistrationView = findViewById(R.id.register_text);
-        mForgotPasswordView = findViewById(R.id.forgot_password_text);
-        mEmailTextView = findViewById(R.id.login_email_text);
-        mPasswordTextView = findViewById(R.id.login_password_text);
-        mLoginBtn = findViewById(R.id.login_btn);
-    }
+        binding = ActivityNavigationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-    private void login() {
-        mLoginBtn.setOnClickListener(v -> {
-            getLoginData();
-            if(mEmail.equals("")|| mPassword.equals("")){
-                Toast.makeText(MainActivity.this,"please fill all the fields",Toast.LENGTH_LONG).show();
-            }else{
-                db.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.hasChild(mEmail)){
-                            String getPass = snapshot.child(mEmail).child("password").getValue(String.class);
-                            assert getPass != null;
-                            if(getPass.equals(mPassword)){
-                                addTrip = new Intent(MainActivity.this,UpcomingPage.class);
-                                startActivity(addTrip);
-                            }else{
-                                mPasswordTextView.setError("wrong password");
-                                mPasswordTextView.requestFocus();
-                            }
-                        }
-                        else{
-                                mEmailTextView.setError("wrong email");
-                                mEmailTextView.requestFocus();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {}
-                });
+        setSupportActionBar(binding.appBarNavigation.toolbar);
+        binding.appBarNavigation.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_upcoming, R.id.nav_history, R.id.nav_historyMap)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation2);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
-    private void getLoginData() {
-        mEmail = mEmailTextView.getText().toString().replace(".",",");
-        mPassword = mPasswordTextView.getText().toString();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
     }
 
-    private void goToForgot() {
-        mForgotPasswordView.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this,ForgetYourPassword.class);
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation2);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id  = item.getItemId();
+        if (id == R.id.nav_upcoming)
+        {
+            Intent intent = new Intent(this, UpcomingPage.class);
             startActivity(intent);
-        });
-    }
+        }
 
-    private void goToRegistration() {
-        mRegistrationView.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this,RegisterPage.class);
-            startActivity(intent);
-        });
+        else if(id == R.id.nav_history)
+                startActivity(new Intent(MainActivity.this, AddNotePage.class));
+        else if (id == R.id.nav_historyMap)
+                startActivity(new Intent(MainActivity.this, LoginPage.class));
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
